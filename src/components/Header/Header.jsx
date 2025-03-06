185b71b5def03385a7ce92cd63cd7902777db280
HectorAgHe <aguilarhector440@gmail.com>
2025-03-06 15:33:31 -0600

    Modicacion header, lista y queries

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,86 @@
-
 import CartIcon, { AccountIcon } from "../Common/Icons/Icons"
 import "./header.css"
 import { useNavigate } from "react-router-dom"
-import { useContext, useEffect, useState } from "react"
+import { useContext, useState } from "react"
 import { userContext } from "../../context/userContext/userContext"
 
-export default function Header (){
-    const {isAuth,userData,isLoading} = useContext(userContext)
-    
+export default function Header() {
+    const { isAuth, userData, isLoading } = useContext(userContext)
     const navigate = useNavigate()
-    return (
-        <div className="header-container" >
-            <h2>E-TIANGUIS</h2>
-            <input className="input-search"  type="text" />
-            <CartIcon color='black' height='45px' width='50px' className='cart-icon' onClick={()=>{
-                if(!isAuth && !userData && !isLoading){
-                 navigate('/selectAccount')
-                }else{
-                 navigate('/cart')
-                }
-            }}/>
-            <div className="account-icon-container" >
-            <AccountIcon color='black' height='55px' width='50px' className='account-icon'  onClick={()=>{
-                if(!isAuth && !userData && !isLoading){
 
-                    navigate('/selectAccount')
-                }else{
-                     navigate('/profile')
-                }
-            }} />
-            <p>{!isAuth && !userData && !isLoading ? 'Ingresar':''}</p>
-         
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
+
+    return (
+        <div className="header-container">
+            <h2>E-TIANGUIS</h2>
+
+            <div className="input-list-container">
+                <input className="input-search" type="text" />
+                <div className="list-container">
+                    <ul>
+                        <button>Categorias</button>
+                        <li>Tendencias</li>
+                        <li>Ofertas</li>
+                        <li>Ayuda</li>
+                    </ul>
+                </div>
             </div>
 
+            <div className="cart-icon">
+                <CartIcon
+                    color="black"
+                    height="45px"
+                    width="50px"
+                    className="cart-iconn"
+                    onClick={() => {
+                        if (!isAuth && !userData && !isLoading) {
+                            navigate('/selectAccount')
+                        } else {
+                            navigate('/cart')
+                        }
+                    }}
+                />
+            </div>
+
+            <div className="account-icon-container">
+                <AccountIcon
+                    color="black"
+                    height="55px"
+                    width="50px"
+                    className="account-icon"
+                    onClick={() => {
+                        if (!isAuth && !userData && !isLoading) {
+                            navigate('/selectAccount')
+                        } else {
+                            navigate('/profile')
+                        }
+                    }}
+                />
+                <p>{!isAuth && !userData && !isLoading ? 'Ingresar' : ''}</p>
+            </div>
+
+            {/* Botón para el menú hamburguesa en pantallas pequeñas */}
+            <div className="hamburger-menu" onClick={toggleMenu}>
+                <button>☰</button>
+            </div>
+
+            {/* Menú móvil */}
+            {isMenuOpen && (
+                <div className="mobile-menu">
+                    <ul>
+                        <li onClick={() => navigate('/cart')}>Carrito</li>
+                        <li onClick={() => navigate('/profile')}>Perfil</li>
+                        <li>Tendencias</li>
+                        <li>Ofertas</li>
+                        <li>Ayuda</li>
+                        <button onClick={() => navigate('/categories')}>Categorías</button>
+                    </ul>
+                </div>
+            )}
         </div>
     )
 }
